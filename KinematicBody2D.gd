387ffb7eed85b1e7aecae020b75e24e1fa4a74df
Lucas <lucaslagoanogueira@hotmail.com>
2018-05-23 17:32:51 -0300

    trabalho rea

---
+++ KinematicBody2D.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+export (int) var speed = 200
+
+var velocity = Vector2()
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	_process(true)
+
+func parado():
+	#altera a animação se o personagem estiver parado.
+	if velocity.x == 0:
+		$AnimatedSprite.animation="parado"
+		$AnimatedSprite.play()
+func get_input():
+    velocity = Vector2()
+    
+    if Input.is_action_pressed("ui_right"):
+       velocity.x += 1
+       $AnimatedSprite.animation="caminhando_dir"
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+        $AnimatedSprite.animation="caminhando_esq"
+    if Input.is_action_pressed('down'):
+        velocity.y += 1
+    if Input.is_action_pressed('up'):
+        velocity.y -= 1
+    velocity = velocity.normalized() * speed
+	
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	#$AnimatedSprite.play()
+	parado()
+	get_input()
+	move_and_slide(velocity)
+	
+
+	
