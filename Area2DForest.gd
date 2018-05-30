69349165d0760272d80c319dc43115868ba7e857
Lucas <lucaslagoanogueira@hotmail.com>
2018-05-30 17:24:22 -0300

    falta transicao de scenes e diario

--- Area2DForest.gd
+++ Area2DForest.gd
@@ -1,4 +1,4 @@
-extends Area2DDesert
+extends Area2D
 
 # class member variables go here, for example:
 var interacao = 0
@@ -10,17 +10,17 @@ func _ready():
 	connect("body_entered",self,"colisao")
 	connect("body_exited",self,"saiu")
 	set_process(true)
-	
+
 
 func colisao(body):
 	print("Entrou na zona de interacao")
 	interacao=1
-	
-	
+
+
 func saiu(body):
 	print("Saiu da zona")
 	interacao=0
-	
+
 func _process(delta):
 	if(interacao and Input.is_action_pressed("ui_accept")):
 		print("Clique")
@@ -31,4 +31,4 @@ func _process(delta):
 #	pass
 func mensagem():
 	get_node("RichTextLabel").percent_visible=1
-	get_node("Bone").visible=1
+	get_node("Sprite").visible=1
