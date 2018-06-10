6a02241e22bf1d680ae47fca86bd640fa0e062e3
exterminus <cmagnobarbosa@gmail.com>
2018-06-09 21:51:50 -0300

    cemiterio

---
+++ NPC3body_Cemiterio.gd
@@ -0,0 +1,18 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var velocity = Vector2()
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	velocity = velocity.normalized() * 50
+	velocity.x+=1
+	move_and_slide(velocity)
